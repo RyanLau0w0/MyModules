841603deeb428cd13ccbcadab59e6181236674bd
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-18 14:53:41 +0800

    Finished creating and declaring variables and onClickListeners

--- app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/MainActivity.java
@@ -3,12 +3,61 @@ package sg.edu.rp.c346.id22023219.mymodules;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvC203;
+    TextView tvC206;
+    TextView tvC218;
+    TextView tvC235;
+    TextView tvC346;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvC203 = findViewById(R.id.textViewC203);
+        tvC206 = findViewById(R.id.textViewC206);
+        tvC218 = findViewById(R.id.textViewC218);
+        tvC235 = findViewById(R.id.textViewC235);
+        tvC346 = findViewById(R.id.textViewC346);
+
+        tvC203.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tvC206.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tvC218.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tvC235.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        tvC346.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
