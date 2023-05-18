6af7d5b12c6a1121b4a37c7b79df7e7a3754ada7
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-18 15:12:18 +0800

    Completed PS without additional

--- app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/ModuleDetailActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/ModuleDetailActivity.java
@@ -2,13 +2,28 @@ package sg.edu.rp.c346.id22023219.mymodules;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ModuleDetailActivity extends AppCompatActivity {
+
+    TextView tvDetails;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_module_detail);
+
+        tvDetails = findViewById(R.id.textViewDetails);
+
+        Intent intentReceived = getIntent();
+        String Module = intentReceived.getStringExtra("Module");
+        String Name = intentReceived.getStringExtra("Name");
+        String Sem = intentReceived.getStringExtra("Sem");
+        String Year = intentReceived.getStringExtra("Year");
+        String Credit = intentReceived.getStringExtra("Credit");
+        String Venue = intentReceived.getStringExtra("Venue");
+        tvDetails.setText("Module Code: " + Module + "\nModule Name: " + Name + "\nAcademic Year: " + Year + "\nSemester: " + Sem + "\nModule Credit: " + Credit + "\nVenue: " + Venue);
     }
 }
