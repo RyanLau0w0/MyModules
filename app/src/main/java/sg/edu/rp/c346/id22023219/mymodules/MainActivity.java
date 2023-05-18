8c36e5218586ee379720d60bea0b2fa7e6b0936b
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-18 15:00:09 +0800

    Completed MainActivity

--- app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023219/mymodules/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.id22023219.mymodules;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -28,35 +29,70 @@ public class MainActivity extends AppCompatActivity {
         tvC203.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+                intent.putExtra("Module" , "C203");
+                intent.putExtra("Name" , "Web Application Development in php");
+                intent.putExtra("Sem" , "1");
+                intent.putExtra("Year" , "2023");
+                intent.putExtra("Credit" , "4");
+                intent.putExtra("Venue" , "W65D");
+                startActivity(intent);
             }
         });
 
         tvC206.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+                intent.putExtra("Module" , "C206");
+                intent.putExtra("Name" , "Software Development Process");
+                intent.putExtra("Sem" , "1");
+                intent.putExtra("Year" , "2023");
+                intent.putExtra("Credit" , "4");
+                intent.putExtra("Venue" , "W65D");
+                startActivity(intent);
             }
         });
 
         tvC218.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+                intent.putExtra("Module" , "C218");
+                intent.putExtra("Name" , "UI/UX Design for Apps");
+                intent.putExtra("Sem" , "1");
+                intent.putExtra("Year" , "2023");
+                intent.putExtra("Credit" , "4");
+                intent.putExtra("Venue" , "W65D");
+                startActivity(intent);
             }
         });
 
         tvC235.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+                intent.putExtra("Module" , "C235");
+                intent.putExtra("Name" , "IT Security and Management");
+                intent.putExtra("Sem" , "1");
+                intent.putExtra("Year" , "2023");
+                intent.putExtra("Credit" , "4");
+                intent.putExtra("Venue" , "W65D");
+                startActivity(intent);
             }
         });
 
         tvC346.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, ModuleDetailActivity.class);
+                intent.putExtra("Module" , "C346");
+                intent.putExtra("Name" , "Mobile App Development");
+                intent.putExtra("Sem" , "1");
+                intent.putExtra("Year" , "2023");
+                intent.putExtra("Credit" , "4");
+                intent.putExtra("Venue" , "E63A");
+                startActivity(intent);
             }
         });
     }
